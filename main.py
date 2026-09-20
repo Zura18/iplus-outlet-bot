a9ca7c894f5363c597dd9079ba810809ab0fa61d
Zura18 <115984477+Zura18@users.noreply.github.com>
2026-09-20 14:46:47 +0400

    Enhance product filtering with brands and storage
    Added lists for brands and storage options, and updated product filtering logic to include checks for brand, storage, and refurbished items.

--- main.py
+++ main.py
@@ -9,11 +9,42 @@ print("Status code:", response.status_code)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-print("\nბმულები:")
+brands = [
+    "iPhone",
+    "Samsung",
+    "Google",
+    "Xiaomi",
+    "OnePlus",
+    "MacBook",
+    "ASUS",
+    "Lenovo",
+    "HP",
+    "Dell",
+    "Acer",
+    "MSI"
+]
+
+storage = [
+    "64GB",
+    "128GB",
+    "256GB",
+    "512GB",
+    "1TB"
+]
+
+print("\nჩვენთვის საინტერესო პროდუქტები:")
 print("-------------------------")
 
 for link in soup.find_all("a"):
+
     name = link.get_text(" ", strip=True)
 
-    if name:
+    if not name:
+        continue
+
+    has_brand = any(brand.lower() in name.lower() for brand in brands)
+    has_storage = any(size.lower() in name.lower() for size in storage)
+    has_refurbished = "refurb" in name.lower()
+
+    if has_brand and has_storage and has_refurbished:
         print(name)
