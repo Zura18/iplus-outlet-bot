2ec89784c3b2c75e0dbbe0c2df6e47277de55ab0
Zura18 <115984477+Zura18@users.noreply.github.com>
2026-09-20 14:33:09 +0400

    Change printed output from text to links

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ url = "https://iplus.com.ge/ka/outlet/"
 response = requests.get(url)
 
 print("Status code:", response.status_code)
-print("HTML length:", len(response.text))
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-print("\nყველა ტექსტური ნაწილი:")
+print("\nბმულები:")
 print("-------------------------")
 
-for tag in soup.find_all(["h1", "h2", "h3", "h4", "h5"]):
-    text = tag.get_text(strip=True)
+for link in soup.find_all("a"):
+    name = link.get_text(" ", strip=True)
 
-    if text:
-        print(text)
+    if name:
+        print(name)
